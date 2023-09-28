27f1bcb172149dbd3fac33b3c80596d8877b5172
Jon Allured <jon@jonallured.com>
2023-09-27 21:39:10 -0500

    Count all cranks
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>

--- main.lua
+++ main.lua
@@ -4,12 +4,12 @@ ticks = 0
 
 function playdate.update()
 	playdate.graphics.clear()
-	local x = 0
-	local y = 0
+	local x = 10
+	local y = 10
 	
-	local newTicks = playdate.getCrankTicks(1)
+	local newTicks = math.abs(playdate.getCrankTicks(1))
 	ticks += newTicks
 	
-	local message = "ticks " .. ticks
+	local message = "cranks " .. ticks
 	playdate.graphics.drawText(message, x, y)
 end
