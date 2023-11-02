ce2ee346b5d41a3d6cd19a5dbe113cc1dd074f6f
Jon Allured <jon@jonallured.com>
2023-11-01 21:45:35 -0500

    Introduce a menu and the qr code mode

--- source/main.lua
+++ source/main.lua
@@ -1,15 +1,32 @@
 import "CoreLibs/crank"
 
+isNormalMode = true
+
+local menu = playdate.getSystemMenu()
+
+local menuItem, error = menu:addMenuItem("normal", function()
+	isNormalMode = true
+end)
+
+local menuItem, error = menu:addMenuItem("code", function()
+	isNormalMode = false
+end)
+
 ticks = 0
 
 function playdate.update()
 	playdate.graphics.clear()
-	local x = 10
-	local y = 10
 	
-	local newTicks = math.abs(playdate.getCrankTicks(1))
-	ticks += newTicks
-	
-	local message = "cranks " .. ticks
-	playdate.graphics.drawText(message, x, y)
+	if isNormalMode then
+		local x = 10
+		local y = 10
+		
+		local newTicks = math.abs(playdate.getCrankTicks(1))
+		ticks += newTicks
+		
+		local message = "cranks " .. ticks
+		playdate.graphics.drawText(message, x, y)
+	else
+		playdate.graphics.drawText("qr code", 0, 0)
+	end
 end
